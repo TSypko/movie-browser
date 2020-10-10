2cea58c350207f2690377b0e978fabe6fe769649
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-10 14:17:33 +0200

    VotesSection component in Tile structure has been prepared

---
+++ src/common/Tile/VotesSection/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledSection } from "./styled";
+
+const VotesSection = () => (
+  <StyledSection>
+    <h1>VotesSection</h1>;
+  </StyledSection>
+);
+
+export default VotesSection;
