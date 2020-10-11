80267a4522bcfecda0327c73516a47737312b59d
Tomasz Sypko <tsypko@gmail.com>
2020-10-11 12:05:37 +0200

    new colors added

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,10 @@
 export const theme = {
     colors: {
+      Mystic: "#E4E6F0",
       Mercury: "#E5E5E5",
+      PattensBlue: "#D6E4FF",
+      MineShaft: "#333333",
+      Woodsmoke: "#18181B",
     },
 
     breakpoints: {
