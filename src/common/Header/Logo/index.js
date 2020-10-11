68f9cc2625a956775ebeba95231ecaeca2d9c0b3
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-11 21:32:50 +0200

    fix logo icon path

--- src/common/Header/Logo/index.js
+++ src/common/Header/Logo/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Icon, LogoWrapper } from "./styled";
-import LogoImage from "../../../assets/images/Logo.svg";
+import LogoImage from "../../../assets/images/logo.svg";
 
 const Logo = () => (
     <LogoWrapper>
