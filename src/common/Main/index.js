f6833bbf975e20d42703e4e737acd468bf6bb7ac
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-11 17:33:52 +0200

    Main component with example content has been created

---
+++ src/common/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { MainContainer } from "./styled";
+
+const Main = () => (
+  <MainContainer>
+    <h1>Example main section</h1>
+  </MainContainer>
+);
+
+export default Main;
---
+++ src/common/Main/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.main`
+  max-width: 1368px;
+  min-height: 1391px;
+  margin: 56px auto 40px auto;
+`;
+
