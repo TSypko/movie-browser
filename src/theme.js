c4f78503769670340bed32f87203b8c52cdec9cc
Tomasz Sypko <tsypko@gmail.com>
2020-10-11 13:11:26 +0200

    added ScienceBlue and Waterloo colors

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,8 @@ export const theme = {
       Mystic: "#E4E6F0",
       Mercury: "#E5E5E5",
       PattensBlue: "#D6E4FF",
+      ScienceBlue: "#0044CC",
+      Waterloo: "#7E839A",
       MineShaft: "#333333",
       Woodsmoke: "#18181B",
     },
