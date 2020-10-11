82555d51245955e8daafa6e326164cd1d0bd93f5
Tomasz Sypko <tsypko@gmail.com>
2020-10-11 13:44:37 +0200

    Added Pagination to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Pagination from './common/Pagination';
 
 function App() {
   return (
     <div>
       Strona główna...
+      <Pagination />
     </div>
   );
 }
