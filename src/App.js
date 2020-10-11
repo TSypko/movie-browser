33c9a631718c50e4cd91a923e8de8f404bdf5797
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-11 12:08:11 +0200

    movieDetails object has been added to App.js for trying to include new props to Tile component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-import Tile from "./common/Tile"; 
+import Tile from "./common/Tile";
+import Poster from "./assets/images/poster.svg";
 
 function App() {
+  const movieDetails = {
+    title: "Mulan",
+    poster: Poster,
+    year: 2020,
+    genres: ["action", "drama", "adventure"],
+    description:
+      "A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.",
+    production: ["China", "USA"],
+    release: "24.10.2020",
+    rate: 7.8,
+    votes: 35,
+  };
+
   return (
     <>
       <div>Strona główna...</div>
