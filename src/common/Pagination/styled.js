fcebc228c74beb59242b95033a95cbd7e05cd1c3
Tomasz Sypko <tsypko@gmail.com>
2020-10-11 14:20:16 +0200

    added media queries

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -19,6 +19,11 @@ export const Button = styled.button`
     color: ${({ theme }) => theme.colors.MineShaft};
     background: ${({ theme }) => theme.colors.PattensBlue};
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            margin: 12px 4px;
+            padding: 8px 12px;
+    }
+
     &:hover {
         filter:brightness(95%);
     }
@@ -37,17 +42,46 @@ export const Button = styled.button`
     }
 `;
 
+export const ButtonText = styled.span`
+        ${({ mobile }) => mobile && css`
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            display: none;
+        }
+    `}
+`;
+
 export const Icon = styled.img`
     height: 11px;
     filter: invert(18%) sepia(100%) saturate(2516%) hue-rotate(213deg) brightness(96%) contrast(103%);
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        height: 8px;
+        }
+
+    ${({ mobile }) => mobile && css`
+        display: none;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            display: unset;
+        }
+    `}
+
     ${({ next }) => next && css`
         margin-left: 8px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            margin-left: 4px;
+        }
     `}
 
     ${({ prev }) => prev && css`
         margin-right: 8px;
         transform: rotate(180deg);
+        
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            margin-right: 4px;
+        }
     `}
 
     ${({ disabled }) => disabled && css`
@@ -57,12 +91,21 @@ export const Icon = styled.img`
 
 export const Paragraph = styled.p`
     margin: 12px 16px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            font-size: 10px;
+            margin: 12px 4px;
+    }
 `;
 
 export const PageText = styled.span`
     line-height: 1.5;
     margin: 0px 4px;
     color: ${({ theme }) => theme.colors.Waterloo};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            margin: 0px 1px;
+    }
 `;
 
 export const PageCounter = styled.span`
@@ -70,4 +113,8 @@ export const PageCounter = styled.span`
     margin: 0px 4px;
     font-weight: 600;
     color: ${({ theme }) => theme.colors.Woodsmoke};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+            margin: 0px 1px;
+    }
 `;
