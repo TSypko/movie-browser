eb89c30bd17967e0f09c78a7ae7748522b078e67
Tomasz Sypko <tsypko@gmail.com>
2020-10-11 13:12:07 +0200

    Added Icons and Text to Pagination

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import { Button } from './styled';
-
+import icon from "./Icon.svg";
+import { Footer, Button, Icon, Paragraph, PageText, PageCounter } from './styled';
 
 const Pagination = () => {
     return (
-        <footer>
+        <Footer>
             <Button disabled>
+                <Icon
+                    prev
+                    disabled
+                    src={icon}
+                    alt="back to first page symbol"
+                />
                 First
             </Button>
             <Button disabled>
+                <Icon
+                    prev
+                    disabled
+                    src={icon}
+                    alt="back to previous page symbol"
+                />
                 Previous
             </Button>
-            {/* <Paragraph>
-                Page 1 of 500
-            </Paragraph> */}
-            <Button>
+            <Paragraph>
+                <PageText>
+                    Page
+                </PageText>
+                <PageCounter>
+                    1
+                </PageCounter>
+                <PageText>
+                    of
+                </PageText>
+                <PageCounter>
+                    500
+                </PageCounter>
+            </Paragraph>
+            <Button >
                 Next
+                <Icon
+                    next
+                    src={icon}
+                    alt="move to next page symbol"
+                />
             </Button>
             <Button>
                 Last
+                <Icon
+                    next
+                    src={icon}
+                    alt="move to last page symbol"
+                />
             </Button>
-        </footer>
+        </Footer>
     )
 }
 
