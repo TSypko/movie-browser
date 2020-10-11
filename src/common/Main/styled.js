7e8086992817ca2c7222147aff47728abd5f30e7
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-11 21:14:41 +0200

    min-height has been removed from Main component

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const MainContainer = styled.main`
   max-width: 1368px;
-  min-height: 1391px;
   margin: 56px auto 40px auto;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
