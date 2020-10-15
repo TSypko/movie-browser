62b4e5721693bec5b0b9d41fe25544adbe7efb35
Tomasz Sypko <tsypko@gmail.com>
2020-10-15 10:41:40 +0200

    added fetch function

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,12 +1,11 @@
 import { takeLatest, call, put, delay } from "redux-saga/effects";
+import { getPopularMovies } from "../../apiClient";
 import { fetchPopularMovies, fetchPopularMoviesSucces, fetchPopularMoviesError } from "./moviesSlice";
 
 function* fetchPopularMoviesHandler() {
     try {
         yield delay(500);
-        const popularMovies = yield call(
-            // fetchFunctionHere
-        );
+        const popularMovies = yield call(getPopularMovies);
         yield put(fetchPopularMoviesSucces(popularMovies));
     } catch (error) {
         yield put(fetchPopularMoviesError());
