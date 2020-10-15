2b3468588d7393a776afe336fbe293ae1dc8f91a
Tomasz Sypko <tsypko@gmail.com>
2020-10-15 14:18:52 +0200

    Updates

---
+++ precache-manifest.ee0b7544d2bae4b0d25fecef09fe5235.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae23276219bc6f59e562f24f47abeb5e",
+    "url": "/movie-browser/index.html"
+  },
+  {
+    "revision": "700d7a09a403a89f7636",
+    "url": "/movie-browser/static/js/2.edeb0935.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-browser/static/js/2.edeb0935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42217e7f416558a9323",
+    "url": "/movie-browser/static/js/main.9d0a7621.chunk.js"
+  },
+  {
+    "revision": "940b2a428110daf5a21e",
+    "url": "/movie-browser/static/js/runtime-main.6da0a001.js"
+  },
+  {
+    "revision": "cba12d72a04cde68e74c1231cd617af1",
+    "url": "/movie-browser/static/media/arrowIcon.cba12d72.svg"
+  },
+  {
+    "revision": "cdf24f103dd27027831a744b87c4311d",
+    "url": "/movie-browser/static/media/logo.cdf24f10.svg"
+  },
+  {
+    "revision": "1338edf11223432d71594f6c7e94da9c",
+    "url": "/movie-browser/static/media/poster.1338edf1.svg"
+  },
+  {
+    "revision": "9a86aa045b54a4e15d1385f113554ef2",
+    "url": "/movie-browser/static/media/search.9a86aa04.svg"
+  },
+  {
+    "revision": "b489e61393b0ed1bb8e71ee82f0e4821",
+    "url": "/movie-browser/static/media/star.b489e613.svg"
+  }
+]);
