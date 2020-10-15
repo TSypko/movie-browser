a801f73a352fc4e5d96629bb2ab6da3c3085deb6
Tomasz Sypko <tsypko@gmail.com>
2020-10-15 13:50:21 +0200

    GenreSection component refactored to use API data

--- src/common/Tile/GenreSection/index.js
+++ src/common/Tile/GenreSection/index.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import GenreTile from "../GenreTile";
 import { StyledSection } from "./styled";
+import { selectGenres } from "../../../features/movies/moviesSlice"
 
-const GenreSection = ({ horizontal, genres }) => (
-  <StyledSection horizontal={horizontal}>
-    {genres?.map((genre) => (
-      <GenreTile key={genre} horizontal={horizontal} genre={genre} />
-    ))}
-  </StyledSection>
-);
+const GenreSection = ({ horizontal, genres }) => {
+
+  const genresList = useSelector(selectGenres);
+
+  return (
+    <StyledSection horizontal={horizontal}>
+      {genresList.genres && genres?.map((genre) =>
+        <GenreTile
+          key={genre}
+          horizontal={horizontal}
+          genre={
+            genresList.genres[(genresList.genres?.findIndex(({ id }) => id === genre))].name
+          } />
+      )}
+    </StyledSection>
+  );
+};
 
 export default GenreSection;
