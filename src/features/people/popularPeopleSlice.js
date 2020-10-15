8b489bf14e164e3e63828fd7fb88fe6826e3d40c
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-15 18:24:55 +0200

    getPopularPeople for Saga and setPopularPeople for set state have been added to popularPeopleSlice, selector for popular people has been created and exported

--- src/features/people/popularPeopleSlice.js
+++ src/features/people/popularPeopleSlice.js
@@ -2,6 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const popularPeopleSlice = createSlice({
   name: "popularPeople",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    page: 1,
+    totalPages: 500,
+    popularPeobleByPage: [],
+  },
+  reducers: {
+    getPopularPeople: () => {
+      console.log("Method for Saga");
+    },
+
+    setPopularPeople: (state, action) => {
+      state.page = action.payload.page;
+      state.totalPages = action.payload.total_pages;
+      state.popularPeobleByPage = action.payload.results;
+    },
+  },
 });
+
+export const selectPopularPeople = state => state.popularPeople.popularPeobleByPage;
+export const {getPopularPeople, setPopularPeople} = popularPeopleSlice.actions;
+export const popularPeopleReducer = popularPeopleSlice.reducer;
