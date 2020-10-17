dc9dff72e797e48dcd3acce43b17d021175ab503
Tomasz Sypko <tsypko@gmail.com>
2020-10-17 18:29:52 +0200

    added local state for buttons

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectMovies } from '../../features/movies/moviesSlice';
 import Button from "./Button";
 import Icon from "./Icon";
 import PageInfo from './PageInfo';
 import { Footer } from './styled';
 
 const Pagination = ({ type }) => {
+
+  const movies = useSelector(selectMovies);
+
+  const [backButtonState, setBackButtonState] = useState(false);
+  const [forwardButtonState, setForwardButtonState] = useState(false);
+
+  useEffect(() => {
+    if (movies.page === 1) {
+      setBackButtonState(true);
+    }
+    else if (movies.page === movies.total_pages) {
+      setForwardButtonState(true);
+    };
+  }, [movies.page, movies.total_pages]);
+
   return (
     <Footer>
       <Button
         previous
         buttonText="First"
-        disabled={true}
+        disabled={backButtonState}
         body={
           <>
             <Icon
               mobile
               previous
-              disabled={true}
+              disabled={backButtonState}
               alt="back to first page symbol"
             />
             <Icon
               previous
-              disabled={true}
+              disabled={backButtonState}
               alt="back to previous page symbol"
             />
           </>}
@@ -29,42 +46,42 @@ const Pagination = ({ type }) => {
       <Button
         previous
         buttonText="Previous"
-        disabled={true}
+        disabled={backButtonState}
         body={
           <>
             <Icon
               previous
-              disabled={true}
+              disabled={backButtonState}
               alt="back to first page symbol"
             />
           </>}
       />
 
-      <PageInfo type={type}/>
+      <PageInfo type={type} />
 
       <Button
         buttonText="Next"
-        disabled={false}
+        disabled={forwardButtonState}
         body={
           <>
             <Icon
-              disabled={false}
+              disabled={forwardButtonState}
               alt="move to next page symbol"
             />
           </>}
       />
       <Button
         buttonText="Last"
-        disabled={false}
+        disabled={forwardButtonState}
         body={
           <>
             <Icon
-              disabled={false}
+              disabled={forwardButtonState}
               alt="move to last page symbol"
             />
             <Icon
               mobile
-              disabled={false}
+              disabled={forwardButtonState}
               alt="move to last page symbol"
             />
           </>}
