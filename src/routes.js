dd6795af1a3a9905596f91506eee4ceb1033df19
Tomasz Sypko <tsypko@gmail.com>
2020-10-17 23:00:43 +0200

    added new routes

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,4 @@
-export const toMovies = () => "/movies";
-export const toPeople = () => "/people";
+export const toMoviesPage = () => "/movies/:page";
+export const toMovies = () => "/movies/1";
+export const toPeoplePage = () => "/people/:page";
+export const toPeople = () => "/people/1";
