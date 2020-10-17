5625aefec3f6d2a4e3f694c575c2ce6c611e20c4
Tomasz Sypko <tsypko@gmail.com>
2020-10-17 22:59:32 +0200

    added conditions to display sutiable data

--- src/common/Pagination/PageInfo/index.js
+++ src/common/Pagination/PageInfo/index.js
@@ -1,29 +1,51 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { selectMovies } from '../../../features/movies/moviesSlice';
+import { selectPopularPeople } from '../../../features/people/popularPeopleSlice';
 import { PageCaption, Paragraph } from './styled';
 
 const PageInfo = ({ type }) => {
 
     const movies = useSelector(selectMovies);
+    const people = useSelector(selectPopularPeople);
 
     return (
+
         <Paragraph>
-            <PageCaption>
-                Page
-            </PageCaption>
-            <PageCaption number>
-                {type === "movies" && movies.page}
-                {/* {type === "people" && people?.page} */}
-            </PageCaption>
-            <PageCaption>
-                of
-            </PageCaption>
-            <PageCaption number>
-                {type === "movies" && movies.total_pages}
-                {/* {type === "people" && people?.total_pages} */}
-            </PageCaption>
+            { movies && type === "movies" &&
+                <>
+                    <PageCaption>
+                        Page
+                    </PageCaption>
+                    <PageCaption number>
+                        {movies.page}
+                    </PageCaption>
+                    <PageCaption>
+                        of
+                    </PageCaption>
+                    <PageCaption number>
+                        {movies.total_pages}
+                    </PageCaption>
+                </>
+            }
+            { people && type === "people" &&
+                <>
+                    <PageCaption>
+                        Page
+                    </PageCaption>
+                    <PageCaption number>
+                        {people.page}
+                    </PageCaption>
+                    <PageCaption>
+                        of
+                    </PageCaption>
+                    <PageCaption number>
+                        {people.total_pages}
+                    </PageCaption>
+                </>
+            }
         </Paragraph>
+
     )
 };
 
