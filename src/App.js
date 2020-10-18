5d3a034cda08e2cbce9bdd5ae6a77897cd6bd096
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-18 15:35:56 +0200

    add movie page to router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import MoviesPage from "./features/movies/MoviesPage";
 import PeoplePage from "./features/people/PeoplePage";
 import Header from "./common/Header";
-import { toMovies, toPeople } from "./routes";
+import { toMovies, toMovie, toPeople } from "./routes";
+import ErrorPage from "./common/ErrorPage";
+import MoviePage from "./features/movies/MoviePage";
 
 function App() {
   return (
     <HashRouter>
       <Header />
       <Switch>
+        <Route path={toMovie()}>
+          <MoviePage />
+        </Route>
         <Route path={toMovies()}>
           <MoviesPage />
         </Route>
