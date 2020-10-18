4d9b44af66779a4bc55a11261a3ea13a4f3c061c
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-18 20:46:56 +0200

    mobile styles for horizontal view have been prepared

--- src/common/PeopleTile/index.js
+++ src/common/PeopleTile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyledTile,
   Poster,
@@ -11,32 +11,51 @@ import {
   PersonDescription,
 } from "./styled";
 import PeoplePoster from "../../assets/images/peoplePosterBigExample.svg";
+import { theme } from "../../theme";
 
-const PeopleTile = ({ horizontal }) => (
-  <StyledTile horizontal={horizontal}>
-    <Poster horizontal={horizontal} src={PeoplePoster} alt="actor photo" />
-    <PersonDetails horizontal={horizontal}>
-      <PersonName horizontal={horizontal}>Jason Scott Lee Long name</PersonName>
-      {horizontal ? (
-        <>
-          <PersonBirthDate>
-            <InfoLabelSpan>Date of birth: </InfoLabelSpan>
-            <InfoSpan>25.08.1987</InfoSpan>
-          </PersonBirthDate>
-          <PersonBirthPlace>
-            <InfoLabelSpan>Place of birth: </InfoLabelSpan>
-            <InfoSpan>Wuhan, Hubei, China</InfoSpan>
-          </PersonBirthPlace>
-          <PersonDescription>
-            Liu Yifei was born in Wuhan, Hubei, Province of China on August
-            25th, 1987. She began modeling at the age of 8 and was trained in
-            singing, dancing and the piano. Moving to the United States at 10
-            with her mother, Liu lived there for four years.
-          </PersonDescription>
-        </>
+const PeopleTile = ({ horizontal }) => {
+  const [mobileContent, setMobileContent] = useState(false);
+  const mobileBreakpoint = +theme.breakpoints.mobileMax.slice(0, -2);
+  const onWidthChange = () => {
+    if (window.innerWidth <= mobileBreakpoint) {
+      setMobileContent(true);
+    } else {
+      setMobileContent(false);
+    }
+  };
+
+  useEffect(() => {
+    onWidthChange();
+    window.addEventListener("resize", onWidthChange);
+    return () => window.removeEventListener("resize", onWidthChange);
+  });
+
+  return (
+    <StyledTile horizontal={horizontal}>
+      <Poster horizontal={horizontal} src={PeoplePoster} alt="actor photo" />
+      <PersonDetails horizontal={horizontal}>
+        <PersonName horizontal={horizontal}>Liu Yifei</PersonName>
+        {horizontal ? (
+          <>
+            <PersonBirthDate>
+              <InfoLabelSpan>{mobileContent ? "Birth" : "Date of birth"}: </InfoLabelSpan>
+              <InfoSpan>25.08.1987</InfoSpan>
+            </PersonBirthDate>
+            <PersonBirthPlace>
+              <InfoLabelSpan>Place of birth: </InfoLabelSpan>
+              <InfoSpan>Wuhan, Hubei, China</InfoSpan>
+            </PersonBirthPlace>
+            {horizontal && !mobileContent ? (
+              <PersonDescription>Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</PersonDescription>
+            ) : null}
+          </>
+        ) : null}
+      </PersonDetails>
+      {horizontal && mobileContent ? (
+        <PersonDescription>Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</PersonDescription>
       ) : null}
-    </PersonDetails>
-  </StyledTile>
-);
+    </StyledTile>
+  );
+};
 
 export default PeopleTile;
--- src/common/PeopleTile/styled.js
+++ src/common/PeopleTile/styled.js
@@ -25,6 +25,18 @@ export const StyledTile = styled.div`
       width: auto;
       padding: 40px;
       border-radius: 0;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        display: grid;
+        grid-template-areas:
+          "poster personDetails"
+          "personDescription personDescription";
+        grid-template-columns: 124px 1fr;
+        width: 290px;
+        padding: 16px;
+        box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+        margin: 0 auto;
+      }
       `
   }
 `;
@@ -42,6 +54,16 @@ export const Poster = styled.img`
     css`
       width: 399px;
       margin-right: 20px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.headerMax}) {
+        width: 300px;
+        align-self: flex-start;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        grid-area: poster;
+        width: 120px;
+      }
     `};
 `;
 
@@ -59,6 +81,11 @@ export const PersonDetails = styled.section`
     css`
       margin-top: 0;
       margin-left: 20px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        grid-area: personDetails;
+        margin-left: 16px;
+      }
     `};
 `;
 
@@ -82,6 +109,13 @@ export const PersonName = styled.h3`
       font-size: 36px;
       font-weight: 600;
       line-height: 1.2;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 1.3;
+        margin: 0 0 2px 0;
+      } ;
     `};
 `;
 
@@ -91,24 +125,50 @@ export const InfoLabelSpan = styled.span`
   margin-right: 10px;
   font-size: 18px;
   line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 12px;
+    line-height: 1.2;
+    margin-right: 4px;
+  } ;
 `;
 
 export const InfoSpan = styled.span`
   display: inline-block;
   font-size: 18px;
   line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 12px;
+    line-height: 1.3;
+    margin: 4px 0;
+  } ;
 `;
 
 export const PersonBirthDate = styled.p`
   margin: 12px 0 4px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    margin: 0;
+  } ;
 `;
 
 export const PersonBirthPlace = styled.p`
   margin: 4px 0 12px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    margin: 0;
+  } ;
 `;
 
 export const PersonDescription = styled.p`
   margin-top: 12px;
   font-size: 20px;
   line-height: 1.6;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    grid-area: personDescription;
+    font-size: 14px;
+    margin: 16px 0 0 0;
+  }
 `;
