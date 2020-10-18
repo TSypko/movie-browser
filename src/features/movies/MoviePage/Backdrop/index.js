763b060083236e2b63a4863ec2051e47c47701ee
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-18 21:40:44 +0200

    backdrop added to MoviePage

--- src/features/movies/MoviePage/Backdrop/index.js
+++ src/features/movies/MoviePage/Backdrop/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import Star from "../../../../assets/images/star.svg";
 import { BackdropBackground, BackdropContainer as BackdropPoster, InfoBox, BackdropTitle, StarIcon, Rate, RateValue, MaxRate, Votes, CenteredContainer as BackdropContainer } from "./styled";
 
-export default ({ img, title, rate, votes }) => (
+export default ({ movie }) => (
     <BackdropBackground>
-        <BackdropPoster src={img}>
-        <img src={img} style={{visibility: "hidden"}} />
+        <BackdropPoster src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}>
+            {/* hack to give div the height of backdrop image */}
+            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} style={{ visibility: "hidden" }} />
+            
             <InfoBox>
-                <BackdropTitle>{title}</BackdropTitle>
+                <BackdropTitle>{movie.title}</BackdropTitle>
                 <Rate>
                     <StarIcon src={Star} />
-                    <RateValue>{rate}</RateValue><MaxRate>/10</MaxRate>
-                    <Votes>{votes} votes</Votes>
+                    <RateValue>{movie.vote_average}</RateValue><MaxRate>/10</MaxRate>
+                    <Votes>{movie.vote_count} votes</Votes>
                 </Rate>
             </InfoBox>
         </BackdropPoster>
--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchMovie, selectError, selectLoading, selectMovie } from '../moviesSlice';
+import LoadingSpinner from "../../../common/LoadingSpinner";
+import ErrorPage from "../../../common/ErrorPage";
+import Backdrop from './Backdrop';
 
 const MoviePage = () => {
+    const params = useParams();
+
+    const dispatch = useDispatch();
+    const movie = useSelector(selectMovie);
+    const loading = useSelector(selectLoading);
+    const error = useSelector(selectError);
+
+    useEffect(() => {
+        dispatch(fetchMovie(params.id));
+    }, [params]);
+
+    if (loading) return <LoadingSpinner />;
+    if (error) return <ErrorPage />;
+
     return (
-        <div>
-            Movie Page...
-        </div>
+        <>
+            {movie && movie.backdrop_path &&
+                <Backdrop movie={movie}/>
+            }
+        </>
     )
 }
 
