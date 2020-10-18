29359e22c5fd185a60d4e9e147188c334bc3bea7
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-19 00:00:14 +0200

    add redirection to movie page after clicking in movie tile

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPopularMovies, selectMovies, selectLoading, selectError } from "../moviesSlice";
+import { toMovie } from "../../../routes";
 import Main from "../../../common/Main";
 import Section from "../../../common/Section";
 import Tile from "../../../common/Tile";
 import LoadingSpinner from "../../../common/LoadingSpinner";
 import Pagination from "../../../common/Pagination";
+import {MovieLink} from "./styled";
 
 const MoviesPage = () => {
 
@@ -20,8 +22,8 @@ const MoviesPage = () => {
 
   return (
     <>
-      {loading && <LoadingSpinner /> }
-      {error && <div>ERROR!</div> }
+      {loading && <LoadingSpinner />}
+      {error && <div>ERROR!</div>}
       {movies.results &&
         <>
           <Main>
@@ -30,20 +32,22 @@ const MoviesPage = () => {
               title="Popular Movies"
               body={
                 movies.results?.map(movie =>
-                  <Tile
-                    key={movie.id}
-                    title={movie.title}
-                    poster={movie.poster_path}
-                    year={movie.release_date}
-                    description={movie.overview}
-                    genres={movie.genre_ids}
-                    rate={movie.vote_average}
-                    votes={movie.vote_count}
-                  />
+                  <MovieLink to={toMovie(movie)}>
+                    <Tile
+                      key={movie.id}
+                      title={movie.title}
+                      poster={movie.poster_path}
+                      year={movie.release_date}
+                      description={movie.overview}
+                      genres={movie.genre_ids}
+                      rate={movie.vote_average}
+                      votes={movie.vote_count}
+                    />
+                  </MovieLink>
                 )
               } />
           </Main>
-          <Pagination type="movies"/>
+          <Pagination type="movies" />
         </>
       }
     </>
---
+++ src/features/movies/MoviesPage/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const MovieLink = styled(Link)`
+    text-decoration: none;
+`;
