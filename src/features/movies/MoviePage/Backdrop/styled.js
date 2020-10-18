edb9c4a48306faa0b5b8857876e4cee618303617
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-18 22:40:07 +0200

    fix css

--- src/features/movies/MoviePage/Backdrop/styled.js
+++ src/features/movies/MoviePage/Backdrop/styled.js
@@ -18,10 +18,6 @@ export const BackdropContainer = styled.div`
         linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%), 
         linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%), 
         url(${({ src }) => src});
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
-        max-width: 320px;
-    }
 `;
 
 export const HiddenImage = styled.img`
@@ -50,6 +46,7 @@ export const BackdropTitle = styled.h2`
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
         font-size: 24px;
         margin-bottom: 4px;
+        margin-left: 16px;
     }
 `;
 
@@ -70,6 +67,7 @@ export const Rate = styled.div`
         flex-wrap: nowrap;
         justify-content: flex-start;
         align-items: center;
+        padding-left: 16px;
     }
 `;
 
@@ -101,7 +99,6 @@ export const MaxRate = styled.span`
 
 export const Votes = styled.span`
     margin-top: 17px;
-    /* margin-bottom: 56px; */
     font-weight: 400;
     font-size: 16px;
     line-height: 1.2;
@@ -113,6 +110,5 @@ export const Votes = styled.span`
         flex-basis: auto;
         align-self: center;
         margin: 8px;
-        /* margin-bottom: 16px; */
     }
 `;
