1c7e7190c0ddfa29e89cd821c09735878d0a1e11
Tomasz Sypko <tsypko@gmail.com>
2020-10-19 18:03:19 +0200

    routes name changed

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,2 @@
-export const toMoviesPage = () => "/movies/:page";
-export const toMovies = () => "/movies/1";
-export const toPeoplePage = () => "/people/:page";
-export const toPeople = () => "/people/1";
+export const toMovies = () => "/movies";
+export const toPeople = () => "/people";
