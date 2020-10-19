e340b39bb1ad9e7024e37b49f66f374c5da24564
Tomasz Sypko <tsypko@gmail.com>
2020-10-19 18:04:09 +0200

    Pagination component refactored t ouse URLSearchParams as page handler

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import { selectMovies } from '../../features/movies/moviesSlice';
 import { selectPopularPeople } from '../../features/people/popularPeopleSlice';
+import { useQueryParameter, useReplaceQueryParameter } from '../../useQueryParameters';
+import { page as pageName } from "../../queryParamNames";
 import Button from "./Button";
 import Icon from "./Icon";
 import PageInfo from './PageInfo';
@@ -12,14 +13,15 @@ const Pagination = ({ type }) => {
 
   const movies = useSelector(selectMovies);
   const people = useSelector(selectPopularPeople);
-  const params = useParams();
 
-  const [page, setPage] = useState(+params.page);
+  const page = useQueryParameter(pageName);
   const [backButtonIsDisabled, setBackButtonIsDisabled] = useState(false);
   const [forwardButtonIsDisabled, setForwardButtonIsDisabled] = useState(false);
 
+  const replaceQueryParameter = useReplaceQueryParameter();
+
   const backButtonIsDisabledHandler = () => {
-    if (page === 1) {
+    if (+page === 1) {
       setBackButtonIsDisabled(true);
     } else
       setBackButtonIsDisabled(false);
@@ -27,13 +29,14 @@ const Pagination = ({ type }) => {
 
   const forwardButtonIsDisabledHandler = () => {
     if (type === "movies") {
-      if (page === movies.total_pages) {
+      if (+page === 500) {
+        console.log(+page)
         setForwardButtonIsDisabled(true);
       } else
         setForwardButtonIsDisabled(false);
     };
     if (type === "people") {
-      if (page === people.total_pages) {
+      if (+page === people.total_pages) {
         setForwardButtonIsDisabled(true);
       } else
         setForwardButtonIsDisabled(false);
@@ -46,109 +49,113 @@ const Pagination = ({ type }) => {
   }, [page])
 
   const previousButtonHandler = () => {
-    if (page !== 1) {
-      setPage(prevPage => prevPage - 1);
+    if (+page !== 1) {
+      replaceQueryParameter({
+        key: pageName,
+        value: +page - 1
+      });
     };
   };
 
   const firstButtonHandler = () => {
-    setPage(1);
+    replaceQueryParameter({
+      key: pageName,
+      value: 1
+    });
   };
 
   const nextButtonHandler = () => {
-    if (page !== 500) {
-      setPage(prevPage => prevPage + 1);
+      replaceQueryParameter({
+        key: pageName,
+        value: +page + 1
+      });
     };
+
+    const lastButtonHandler = () => {
+      if (type === "movies") {
+        replaceQueryParameter({
+          key: pageName,
+          value: movies.total_pages
+        });
+      }
+      else if (type === "people") {
+        replaceQueryParameter({
+          key: pageName,
+          value: people.total_pages
+        });
+      };
+    };
+
+    return (
+      <Footer>
+        <Button
+          previous
+          buttonText="First"
+          disabled={backButtonIsDisabled}
+          onClick={firstButtonHandler}
+          body={
+            <>
+              <Icon
+                mobile
+                previous
+                disabled={backButtonIsDisabled}
+                alt="back to first page symbol"
+              />
+              <Icon
+                previous
+                disabled={backButtonIsDisabled}
+                alt="back to previous page symbol"
+              />
+            </>}
+        />
+        <Button
+          previous
+          buttonText="Previous"
+          disabled={backButtonIsDisabled}
+          onClick={previousButtonHandler}
+          body={
+            <>
+              <Icon
+                previous
+                disabled={backButtonIsDisabled}
+                alt="back to first page symbol"
+              />
+            </>}
+        />
+
+        <PageInfo type={type} />
+
+        <Button
+          buttonText="Next"
+          disabled={forwardButtonIsDisabled}
+          onClick={nextButtonHandler}
+          body={
+            <>
+              <Icon
+                disabled={forwardButtonIsDisabled}
+                alt="move to next page symbol"
+              />
+            </>}
+        />
+        <Button
+          buttonText="Last"
+          disabled={forwardButtonIsDisabled}
+          onClick={lastButtonHandler}
+          body={
+            <>
+              <Icon
+                disabled={forwardButtonIsDisabled}
+                alt="move to last page symbol"
+              />
+              <Icon
+                mobile
+                disabled={forwardButtonIsDisabled}
+                alt="move to last page symbol"
+              />
+            </>}
+        />
+      </Footer>
+    );
   };
 
-  const lastButtonHandler = () => {
-    if (movies) {
-      setPage(movies.total_pages)
-    }
-    else if (people) {
-      setPage(people.total_pages)
-    };
-  };
-
-  useEffect(() => {
-    if (type === "movies") {
-      window.location.hash = `#/movies/${page}`
-    }
-    else if (type === "people") {
-      window.location.hash = `#/people/${page}`
-    };
-  }, [type, page]);
-
-  return (
-    <Footer>
-      <Button
-        previous
-        buttonText="First"
-        disabled={backButtonIsDisabled}
-        onClick={firstButtonHandler}
-        body={
-          <>
-            <Icon
-              mobile
-              previous
-              disabled={backButtonIsDisabled}
-              alt="back to first page symbol"
-            />
-            <Icon
-              previous
-              disabled={backButtonIsDisabled}
-              alt="back to previous page symbol"
-            />
-          </>}
-      />
-      <Button
-        previous
-        buttonText="Previous"
-        disabled={backButtonIsDisabled}
-        onClick={previousButtonHandler}
-        body={
-          <>
-            <Icon
-              previous
-              disabled={backButtonIsDisabled}
-              alt="back to first page symbol"
-            />
-          </>}
-      />
-
-      <PageInfo type={type} />
-
-      <Button
-        buttonText="Next"
-        disabled={forwardButtonIsDisabled}
-        onClick={nextButtonHandler}
-        body={
-          <>
-            <Icon
-              disabled={forwardButtonIsDisabled}
-              alt="move to next page symbol"
-            />
-          </>}
-      />
-      <Button
-        buttonText="Last"
-        disabled={forwardButtonIsDisabled}
-        onClick={lastButtonHandler}
-        body={
-          <>
-            <Icon
-              disabled={forwardButtonIsDisabled}
-              alt="move to last page symbol"
-            />
-            <Icon
-              mobile
-              disabled={forwardButtonIsDisabled}
-              alt="move to last page symbol"
-            />
-          </>}
-      />
-    </Footer>
-  )
-}
-
-export default Pagination;
+  export default Pagination;
