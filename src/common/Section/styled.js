546d74e3d2b996121740de3495b04a1e6094ef61
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-22 00:44:15 +0200

    change section title margins

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -5,14 +5,14 @@ export const Title = styled.h2`
   line-height: 1.2;
   font-weight: 600;
   color: ${({ theme }) => theme.colors.Woodsmoke};
-  margin: 0 0 24px 0;
+  margin: 64px 0 32px 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     text-align: center;
     font-size: 18px;
     font-weight: 600;
     line-height: 1.2;
-    margin: 0 0 12px 0;
+    margin: 21px 0 12px 0;
   } ;
 `;
 
