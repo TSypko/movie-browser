d512eacfad7246a729a1c83f2cc10c7a2376c5e9
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-22 19:57:38 +0200

    some style corrections have been applied into PersonLink and MovieLink styled components

--- src/features/movies/MoviesPage/styled.js
+++ src/features/movies/MoviesPage/styled.js
@@ -3,4 +3,7 @@ import { Link } from "react-router-dom";
 
 export const MovieLink = styled(Link)`
     text-decoration: none;
+    display:flex;
+    justify-content: center;
+    align-items: flex-start;
 `;
--- src/features/people/PeoplePage/styled.js
+++ src/features/people/PeoplePage/styled.js
@@ -3,4 +3,7 @@ import { Link } from "react-router-dom";
 
 export const PersonLink = styled(Link)`
     text-decoration: none;
+    display:flex;
+    justify-content: center;
+    align-items: flex-start;
 `;
