1fe735253bc59e180be89447442916d1359f5c08
Tomasz Sypko <tsypko@gmail.com>
2020-10-23 13:19:55 +0200

    added Saga handlers to searching for movies

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,6 +1,17 @@
-import { takeLatest, call, put, delay } from "redux-saga/effects";
-import { getPopularMovies, getGenres, getMovie } from "../../../src/apiClient";
-import { fetchPopularMovies, fetchPopularMoviesSucces, fetchPopularMoviesError, setGenres, fetchMovieSucces, fetchMovieError, fetchMovie } from "./moviesSlice";
+import { takeLatest, call, put, delay, debounce } from "redux-saga/effects";
+import { getPopularMovies, getGenres, getMovie, searchForMovies } from "../../../src/apiClient";
+import {
+    fetchPopularMovies,
+    fetchPopularMoviesSucces,
+    fetchPopularMoviesError,
+    setGenres,
+    fetchMovieSucces,
+    fetchMovieError,
+    fetchMovie,
+    searchMoviesByQuery,
+    searchMoviesByQuerySucces,
+    searchMoviesByQueryError
+} from "./moviesSlice";
 
 function* fetchPopularMoviesHandler({ payload }) {
     try {
@@ -13,21 +24,32 @@ function* fetchPopularMoviesHandler({ payload }) {
     } catch (error) {
         yield put(fetchPopularMoviesError());
         console.error(error);
-    }
+    };
 };
 
-function* fetchMovieHandler(action) {
+function* fetchMovieHandler({ payload }) {
     try {
         yield delay(500);
-        const movie = yield call(getMovie, action.payload);
+        const movie = yield call(getMovie, payload);
         yield put(fetchMovieSucces(movie));
     } catch (error) {
         yield put(fetchMovieError());
         console.error(error);
-    }
+    };
+};
+
+function* searchMoviesByQueryHandler({ payload }) {
+    try {
+        const movies = yield call(searchForMovies, payload.page, payload.query);
+        yield put(searchMoviesByQuerySucces(movies));
+    } catch (error) {
+        yield put(searchMoviesByQueryError());
+        console.error(error);
+    };
 };
 
 export function* moviesSaga() {
     yield takeLatest(fetchPopularMovies.type, fetchPopularMoviesHandler);
     yield takeLatest(fetchMovie.type, fetchMovieHandler);
+    yield debounce(600, searchMoviesByQuery.type, searchMoviesByQueryHandler);
 };
