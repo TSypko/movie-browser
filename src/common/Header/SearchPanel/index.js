e66913e680fae0d6eea184a091361843d20894d2
Tomasz Sypko <tsypko@gmail.com>
2020-10-23 13:18:56 +0200

    added interactions to SearchPanel

--- src/common/Header/SearchPanel/index.js
+++ src/common/Header/SearchPanel/index.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { search as searchQueryParamName } from "../../../queryParamNames";
+import { page as pageQueryParamName } from "../../../queryParamNames";
 import { toMovies } from "../../../routes";
+import { useQueryParameter, useReplaceQueryParameter } from "../../../useQueryParameters";
 import { SearchInput } from "./styled";
 
 const SearchPanel = () => {
+
     const location = useLocation();
+    const replaceQueryParameter = useReplaceQueryParameter();
+    const searchQuery = useQueryParameter(searchQueryParamName);
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter({
+            key: pageQueryParamName,
+            value: "",
+        });
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : "",
+        });
+    };
 
     return (
         <SearchInput
-            placeholder={`Search for ${location.pathname.includes(toMovies()) ? "movies": "people"}...`}
+            placeholder={`Search for ${location.pathname.includes(toMovies()) ? "movies" : "people"}...`}
+            value={searchQuery || ""}
+            onChange={onInputChange}
         />
-    )
+    );
 };
 
 export default SearchPanel;
