b1506cf7d64caf32452f1316b92432c4adef344c
Tomasz Sypko <tsypko@gmail.com>
2020-10-25 12:14:52 +0100

    SearchPanel modified to use feature search

--- src/common/Header/SearchPanel/index.js
+++ src/common/Header/SearchPanel/index.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
+import { resetSearchQuery, getSearchQuery } from "../../../features/search/searchSlice";
+import { search as searchQueryParamName } from "../../../queryParamNames";
+import { page as pageQueryParamName } from "../../../queryParamNames";
 import { toMovies } from "../../../routes";
+import { useQueryParameter, useReplaceQueryParameter } from "../../../useQueryParameters";
 import { SearchInput } from "./styled";
 
 const SearchPanel = () => {
+
     const location = useLocation();
+    const dispatch = useDispatch();
+    const replaceQueryParameter = useReplaceQueryParameter();
+    const searchQuery = useQueryParameter(searchQueryParamName);
+
+    useEffect(() => {
+        return () => {
+            dispatch(resetSearchQuery())
+        };
+    }, [dispatch, location.pathname])
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter({
+            key: pageQueryParamName,
+            value: "",
+        });
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value,
+        });
+        dispatch(getSearchQuery(target.value))
+    }
 
     return (
         <SearchInput
-            placeholder={`Search for ${location.pathname.includes(toMovies()) ? "movies": "people"}...`}
+            placeholder={`Search for ${location.pathname.includes(toMovies()) ? "movies" : "people"}...`}
+            value={searchQuery || ""}
+            onChange={onInputChange}
         />
-    )
+    );
 };
 
 export default SearchPanel;
