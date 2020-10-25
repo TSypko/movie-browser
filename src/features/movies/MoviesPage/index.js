34069f07ff218b3267840084be43ab1cdd601147
Tomasz Sypko <tsypko@gmail.com>
2020-10-25 12:15:01 +0100

    MoviesPage modified to use feature search

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchPopularMovies, selectMovies, selectLoading, selectError } from "../moviesSlice";
+import {
+  fetchPopularMovies,
+  selectMovies,
+  selectLoading,
+  selectError,
+  searchMoviesByQuery,
+} from "../moviesSlice";
 import { page as pageParameterName } from "../../../queryParamNames";
 import Main from "../../../common/Main";
 import Section from "../../../common/Section";
@@ -11,48 +17,72 @@ import { MovieLink } from "./styled";
 import ErrorPage from "../../../common/ErrorPage";
 import { useQueryParameter } from "../../../useQueryParameters";
 import { toMovie } from "../../../routes";
+import NoResultsPage from "../../../common/NoResultsPage";
+import { selectSearchQuery } from "../../search/searchSlice";
 
 const MoviesPage = () => {
 
   const dispatch = useDispatch();
-  const query = useQueryParameter(pageParameterName);
+  const pageQuery = useQueryParameter(pageParameterName);
+  const searchQuery = useSelector(selectSearchQuery);
   const movies = useSelector(selectMovies);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
 
   useEffect(() => {
-    dispatch(fetchPopularMovies(query || 1));
-  }, [dispatch, query]);
+    if (searchQuery) {
+      dispatch(searchMoviesByQuery({
+        page: pageQuery,
+        query: searchQuery
+      }))
+    } else {
+      dispatch(fetchPopularMovies(pageQuery))
+    };
+  
+  }, [dispatch, pageQuery, searchQuery]);
 
   return (
     <>
+    {!loading && !error && movies.total_results === 0 &&
+        <Main>
+          <Section
+            title={`Sorry, there are no results for "${searchQuery}"`}
+            body={<NoResultsPage />}
+          />
+        </Main>}
       {!movies.results && loading && <LoadingSpinner />}
       {!movies.results && error && <ErrorPage />}
-      {movies.results &&
+      
+      {movies.results && movies.total_results !== 0 &&
         <>
           <Main>
             <Section
               type="movies"
-              grid
-              title="Popular Movies"
+              grid={!loading}
+              title={searchQuery
+                ? `Search results for "${searchQuery}"`
+                : loading
+                  ? ""
+                  : "Popular Movies"}
               body={
-                movies.results?.map(movie =>
-                  <MovieLink key={movie.id} to={toMovie(movie)}>
-                    <Tile
-                      key={movie.id}
-                      title={movie.title}
-                      poster={movie.poster_path}
-                      year={movie.release_date}
-                      description={movie.overview}
-                      genres={movie.genre_ids}
-                      rate={movie.vote_average}
-                      votes={movie.vote_count}
-                    />
-                  </MovieLink>
-                )
+                loading ? <LoadingSpinner /> :
+                  movies.results?.map(movie =>
+                    <MovieLink key={movie.id} to={toMovie(movie)}>
+                      <Tile
+                        key={movie.id}
+                        title={movie.title}
+                        poster={movie.poster_path}
+                        year={movie.release_date}
+                        description={movie.overview}
+                        genres={movie.genre_ids}
+                        rate={movie.vote_average}
+                        votes={movie.vote_count}
+                      />
+                    </MovieLink>
+                  )
               } />
           </Main>
-          <Pagination type="movies" />
+          {loading ? null : <Pagination type="movies" />}
         </>
       }
     </>
