5d444f879e508890045ed82c0b8d96d9d2a9084c
Tomasz Sypko <tsypko@gmail.com>
2020-10-26 11:59:26 +0100

    added reseting query  function to navigations items

--- src/common/Header/Navigation/index.js
+++ src/common/Header/Navigation/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { toPeople, toMovies } from "../../../routes";
 import { List, StyledNavLink } from "./styled";
+import { resetSearchQuery } from "../../../features/search/searchSlice";
 
 export default () => {
+
+    const dispatch = useDispatch();
+
+    const onClickResetQuery = () => {
+        dispatch(resetSearchQuery())
+    };
+
     return (
         <nav>
             <List>
-                <StyledNavLink to={toMovies()}>Movies</StyledNavLink>
-                <StyledNavLink to={toPeople()}>People</StyledNavLink>
+                <StyledNavLink to={toMovies()} onClick={onClickResetQuery}>Movies</StyledNavLink>
+                <StyledNavLink to={toPeople()} onClick={onClickResetQuery}>People</StyledNavLink>
             </List>
         </nav>
     );
-}
+};
