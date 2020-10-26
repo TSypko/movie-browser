0ed8d8afa7d2f21891823b5cc7d70b7273d7582b
Tomasz Sypko <tsypko@gmail.com>
2020-10-26 15:25:32 +0100

    added new state

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -4,17 +4,21 @@ const searchSlice = createSlice({
     name: "search",
     initialState: {
         searchQuery: "",
+        searchIsOn: false,
     },
     reducers: {
-        getSearchQuery: () => {
-           
+        getSearchQuery: (state) => {
+            state.searchIsOn = true;
         },
         setSearchQuery: (state, {payload: query}) => {
             state.searchQuery = query;
+            state.searchIsOn = false;
         },
         resetSearchQuery: (state) => {
             state.searchQuery = "";
         },
+        getSearchIson: () => { 
+        }
     }
 });
 
@@ -26,5 +30,6 @@ export const {
 
 export const selectSearchState = state => state.searchQuery;
 export const selectSearchQuery = state => selectSearchState(state).searchQuery;
+export const selectSearchIsOn = state => selectSearchState(state).searchIsOn;
 
 export default searchSlice.reducer;
