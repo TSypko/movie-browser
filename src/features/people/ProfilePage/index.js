3b77c90690a2c6b4a9456f74bfcd82c845baaedf
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-27 19:41:38 +0100

    simple test of Person details fetching

--- src/features/people/ProfilePage/index.js
+++ src/features/people/ProfilePage/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPerson } from "../popularPeopleSlice";
 
 const ProfilePage = () => {
     const params = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPerson(params.id))
+    }, [])
+
     console.log(params);
     return (
         <div>
