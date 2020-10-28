dfa802176550ca284a3bfbaa91e4044ea8300337
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-28 21:18:27 +0100

    some minor bugs have been fixed

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put, delay } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
 import {
   fetchPeople,
   fetchPeopleSucces,
@@ -14,17 +14,15 @@ function* fetchPeopleHandler({ payload }) {
   try {
     const page = payload;
     const people = yield call(getPopularPeople, page);
-    yield delay(500);
     yield put(fetchPeopleSucces(people));
   } catch (error) {
     yield put(fetchPeopleError());
     console.error(error);
-  }
-}
+  };
+};
 
 function* fetchPersonHandler(action) {
   try {
-    yield delay(500);
     const person = yield call(getPerson, action.payload);
     const credits = yield call(getPersonCredits, action.payload);
     const genres = yield call(getGenres);
@@ -33,10 +31,10 @@ function* fetchPersonHandler(action) {
   } catch (error) {
     yield put(fetchPersonError());
     console.error(error);
-  }
+  };
 };
 
 export function* peopleSaga() {
   yield takeLatest(fetchPeople.type, fetchPeopleHandler);
   yield takeLatest(fetchPerson.type, fetchPersonHandler);
-}
+};
