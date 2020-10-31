5410d6071f3786865ffe5054474f20cb29b89d55
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-10-31 23:18:21 +0100

    Revert "add padding to main"
    This reverts commit f69a1261ea817e2e24a42e3167980184faafaae5.

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -3,10 +3,8 @@ import styled from "styled-components";
 export const MainContainer = styled.main`
   max-width: 1368px;
   margin: 56px auto 40px auto;
-  padding: 0 20px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     margin: 24px auto 32px auto;
-    padding: 0 10px;
   } ;
 `;
